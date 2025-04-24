fe140f9db8065a88cc7913166bf4db0c6628eb0d
Gen_X <104616772+GenX77@users.noreply.github.com>
2025-04-24 19:30:33 +0200

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', config_views.index, name='home'),  # Ruta raíz que carga index.html
     path('gestion_usuarios/', include('gestion_usuarios.urls')),
     path('gestion_usu/', include('gestion_usu.urls')),
-    path('agenda/', include('agenda.urls')), 
+    # path('agenda/', include('agenda.urls')),  # Esta línea está duplicada
     # Puedes agregar otras apps aquí si lo necesitas
 ]
