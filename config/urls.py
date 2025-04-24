2fff3f19b9b17ed1fc48b3d2ad50733100c506fe
GenX77 <gendersonc@gmail.com>
2025-04-24 12:07:25 +0200

    Corrijo vista hola_mundo o elimino referencia rota

--- config/urls.py
+++ config/urls.py
@@ -6,11 +6,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hola_mundo, name='index'),
-
-    # Incluye la app gestion_usuarios
     path('usuarios/', include('gestion_usuarios.urls', namespace='gestion_usuarios')),
-
-    # Incluye la app gestion_usu
     path('usu/', include('gestion_usu.urls', namespace='gestion_usu')),
 ]
